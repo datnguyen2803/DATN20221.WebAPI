4b136b8cc573f70a3a0581e60c39960cfad2851c
ntdat_homePC <dat10hoa1@gmail.com>
2023-02-16 10:22:26 +0700

    sample with HelloController

---
+++ DataAPI/DataAPI/Configuration/DataAPIConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace DataAPI.Configuration
+{
+    public class DataAPIConfig
+    {
+        public static void Register (HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+
+            //    );
+
+            IHttpRoute defaultRoute = config.Routes.CreateRoute(
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional },
+                null
+                );
+            config.Routes.Add("DefaultApi", defaultRoute);
+        }
+    }
+}
---
+++ DataAPI/DataAPI/Controller/HelloController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DataAPI.Controller
+{
+    public class Pump
+    {
+        public int Id { get; set; }
+        public string Area { get; set; }
+
+    }
+    public class HelloController : ApiController
+    {
+        List<string> pumpList = new List<string> ();
+
+        /**
+         * GET: /api/hello
+         */
+        public List<string> Get()
+        {
+            return pumpList;
+        }
+
+        /**
+         * GET: /api/hello/id
+         */
+        public string Get(int id)
+        {
+            string retPump = pumpList[id];
+            return retPump;
+        }
+
+        public string Get(int id, string area) 
+        {
+            string ret = "pump " + id.ToString() + " from " + area;
+            return ret;
+        }
+
+        /**
+         * POST: /api/hello
+         */
+        public void Post([FromBody] Pump newPump) 
+        {
+            string strNewPump = "pump " + newPump.Id.ToString() + " from " + newPump.Area;
+            Console.WriteLine(pumpList);
+            pumpList.Add(strNewPump);
+            Console.WriteLine(pumpList);
+            Console.WriteLine(newPump.Id.ToString());
+        }
+
+        public void Put(int id, [FromBody] string value) 
+        {
+
+        }
+
+        public void Delete(int id) 
+        {
+
+        }
+
+
+    }
+}
